98135838512233b914a1b9b1b1367869c1055ec7
Aakash Saxena <aakashsaxena@Aakashs-Mac-mini.local>
2024-11-14 19:02:50 +0530

    text phase 10

--- src/features/Nutrition/MealPlanner/CustomiseIngredients.js
+++ src/features/Nutrition/MealPlanner/CustomiseIngredients.js
@@ -1,15 +1,4 @@
-import React, { useEffect, useState } from 'react';
-import { shallowEqual, useSelector } from 'react-redux';
-import IngredientOption from './Components/IngredientOption';
-import * as Selectors from './Redux/selectors';
-import { IoIosArrowUp, IoIosArrowDown } from 'react-icons/io';
-
-import {
-  Accordion,
-  AccordionSummary,
-  AccordionDetails,
-  Typography,
-} from '@mui/material';
+import React from 'react';
 
 function CustomiseIngredients() {
   return <div className="my-11 h-full w-full"></div>;
