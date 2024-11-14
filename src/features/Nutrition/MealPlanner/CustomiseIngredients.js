4bf31b55a67bec467e3b30980cbe47c00edcc507
Aakash Saxena <aakashsaxena@Aakashs-Mac-mini.local>
2024-11-14 21:13:40 +0530

    text phase 21

--- src/features/Nutrition/MealPlanner/CustomiseIngredients.js
+++ src/features/Nutrition/MealPlanner/CustomiseIngredients.js
@@ -20,11 +20,57 @@ function CustomiseIngredients() {
     selectQuestionSectionInfo,
     shallowEqual,
   );
-  const screen = questionSectionInfo?.screen;
+  const screen = questionSectionInfo.screen;
 
-  const calorie = nutritionPlan?.calorie;
+  const { calorie } = nutritionPlan;
 
-  return <div className="my-11 h-full w-full"></div>;
+  const handleAccordionToggle = (category) => {
+    setShowMoreItems(false);
+    setExpandedAccordion((prevCategory) =>
+      prevCategory === category ? null : category,
+    );
+  };
+
+  useEffect(() => {
+    if (suggestedIngredients) {
+      setExpandedAccordion('protein');
+    }
+  }, [suggestedIngredients]);
+
+  return (
+    <div className="my-11 h-full w-full">
+      {screen === 4 && (
+        <div className="flex h-full w-full flex-col justify-start gap-[6rem]">
+          <section className="flex w-full flex-col items-start justify-center gap-2">
+            <h5 className="text-[18px]" style={{ lineHeight: '23px' }}>
+              Suggested calories in a day
+            </h5>
+            <h2
+              className="text-[32px] text-[#F5C563]"
+              style={{ lineHeight: '40px' }}
+            >
+              {calorie}
+            </h2>
+          </section>
+        </div>
+      )}
+      {screen === 5 && (
+        <div className="flex h-full w-full flex-col justify-start gap-7">
+          <div className="flex w-full flex-col gap-1">
+            <h2 className="text-[18px]" style={{ lineHeight: '23px' }}>
+              Customize your ingredients
+            </h2>
+            <p
+              className="text-[16px] text-[#929292]"
+              style={{ lineHeight: '20px', fontWeight: 400 }}
+            >
+              Uncheck ingredients you don’t want in your meal plan
+            </p>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default CustomiseIngredients;
