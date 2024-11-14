fcd0014fd08f063731fcef3b4eaeafa38cde2a4a
Aakash Saxena <aakashsaxena@Aakashs-Mac-mini.local>
2024-11-14 18:59:27 +0530

    text phase 9

--- src/features/Nutrition/MealPlanner/CustomiseIngredients.js
+++ src/features/Nutrition/MealPlanner/CustomiseIngredients.js
@@ -12,25 +12,6 @@ import {
 } from '@mui/material';
 
 function CustomiseIngredients() {
-  const [expandedAccordion, setExpandedAccordion] = useState(null);
-
-  const selectSuggestedIngredients = Selectors.makeGetSuggestedIngredients();
-  const selectNutritionPlan = Selectors.makeGetNutritionPlan();
-  const selectQuestionSectionInfo = Selectors.makeGetQuestionSectionInfo();
-  const [showMoreItems, setShowMoreItems] = useState(false);
-  const suggestedIngredients = useSelector(
-    selectSuggestedIngredients,
-    shallowEqual,
-  );
-  const nutritionPlan = useSelector(selectNutritionPlan, shallowEqual);
-  const questionSectionInfo = useSelector(
-    selectQuestionSectionInfo,
-    shallowEqual,
-  );
-  const screen = questionSectionInfo.screen;
-
-  const { calorie } = nutritionPlan;
-
   return <div className="my-11 h-full w-full"></div>;
 }
 
