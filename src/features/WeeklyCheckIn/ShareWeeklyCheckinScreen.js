a26f3c4e2e16dbb8d08f501309b0b228074b7b89
Aakash Saxena <aakashsaxena@Aakashs-Mac-mini.local>
2024-12-05 22:57:17 +0530

    bug fix

--- src/features/WeeklyCheckIn/ShareWeeklyCheckinScreen.js
+++ src/features/WeeklyCheckIn/ShareWeeklyCheckinScreen.js
@@ -78,9 +78,9 @@ const ShareWeeklyCheckinScreen = ({
               <div className="w-fit rounded   bg-white-opacity-08 px-[6px]  text-[14px] font-extralight text-blue">
                 {week ? convertToWeekFormat(week) : 'Week Data Unavailable'}
               </div>
-              <h5 className="mt-[2px] text-[20px] leading-[32px] text-offwhite">
+              {/* <h5 className="mt-[2px] text-[20px] leading-[32px] text-offwhite">
                 Hi {fullName || 'User'}, <br /> Here’s your week in Numbers
-              </h5>
+              </h5> */}
             </div>
           </div>
 
