e2e26f1cbbd82829ce758f0ff033420aeef4b7c6
Aakash Saxena <aakashsaxena@Aakashs-Mac-mini.local>
2024-12-10 21:39:04 +0530

    fix workout text

--- src/features/WeeklyCheckIn/ShareWeeklyCheckinScreen.js
+++ src/features/WeeklyCheckIn/ShareWeeklyCheckinScreen.js
@@ -97,8 +97,9 @@ const ShareWeeklyCheckinScreen = ({
                         : 0}
                     </span>{' '}
                     workout
-                    {/* {weeklyReport?.last4WeekConsistency[0]?.count !== 1 &&
-                      's'}{' '} */}
+                    {weeklyReport?.last4WeekConsistency[0]?.count &&
+                      weeklyReport?.last4WeekConsistency[0]?.count !== 1 &&
+                      's'}{' '}
                     last week
                   </p>
                   <div className="flex items-center gap-1">
